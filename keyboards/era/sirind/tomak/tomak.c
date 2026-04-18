2248977fc215291b3e11de4cc119fdea2e10833d
WOO YOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2026-04-19 07:36:12 +0900

    Tomak TKL Vial - Seperated Badge Indicator Control

--- keyboards/era/sirind/tomak/tomak.c
+++ keyboards/era/sirind/tomak/tomak.c
@@ -7,10 +7,6 @@ bool rgb_matrix_indicators_kb(void) {
     if (!rgb_matrix_indicators_user()) {
          return false;
     }
-    if (host_keyboard_led_state().caps_lock) {
-        rgb_matrix_set_color(96, 0, 128, 128);
-        rgb_matrix_set_color(97, 0, 128, 128);
-        rgb_matrix_set_color(98, 0, 128, 128);
-    }
+    
     return true;
 }
